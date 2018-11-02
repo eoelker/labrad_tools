d87e160af1ddbf661e407f5fbef5a07003cfd657
Eric <ericoelker@gmail.com>
2018-11-01 21:11:03 -0600

    Fixed PMT Client

--- pmt/clients/pmt_client.py
+++ pmt/clients/pmt_client.py
@@ -80,11 +80,11 @@ class PMTViewer(QtGui.QDialog):
             print message_type, message
             device_message = message.get(self.pmt_name)
             if (message_type == 'record') and (device_message is not None):
-                self.replot(device_message)
-            elif (message_type == 'record'):
                 rdp = getattr(self, 'raw_data_path')
                 if rdp:
                     self.replot(rdp)
+                else:
+                    self.replot(device_message)
 
     def replot(self, rel_data_path):
         abs_data_path = os.path.join(self.data_dir, rel_data_path) + '.hdf5'
@@ -101,3 +101,108 @@ class PMTViewer(QtGui.QDialog):
     
     def closeEvent(self, x):
         self.reactor.stop()
+        
+class PMTViewerNumeric(PMTViewer):
+
+    """ Class is modified to also display numeric readout of ground, excited,
+    background and total counts.  These values update every time the plot is updated. 
+    """
+
+    def populate(self):
+        self.setWindowTitle(self.pmt_name)
+        self.canvas = MplCanvas()
+        self.nav = NavigationToolbar(self.canvas, self)
+        self.lcdGND = QtGui.QLCDNumber(self)
+        self.lcdGND.display('0.001')
+        self.lcdEXC = QtGui.QLCDNumber(self)
+        self.lcdEXC.display('0.001')
+        self.lcdBAC = QtGui.QLCDNumber(self)
+        self.lcdBAC.display('0.000')
+        self.lcdFRAC = QtGui.QLCDNumber(self)
+        self.lcdFRAC.display('0.000')
+        self.lcdTOTAL = QtGui.QLCDNumber(self)
+        self.lcdTOTAL.display('0.000')
+        self.labelGND = QtGui.QLabel('GROUND')
+        self.labelGND.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelGND.setFont(QtGui.QFont("Arial", 48, QtGui.QFont.Bold))
+        self.labelEXC = QtGui.QLabel('EXCITED')
+        self.labelEXC.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelEXC.setFont(QtGui.QFont("Arial", 48, QtGui.QFont.Bold))
+        self.labelBAC = QtGui.QLabel('BACKGROUND')
+        self.labelBAC.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelBAC.setFont(QtGui.QFont("Arial", 48, QtGui.QFont.Bold))
+        self.labelFRAC = QtGui.QLabel('EXC. FRACTION')
+        self.labelFRAC.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelFRAC.setFont(QtGui.QFont("Arial", 48, QtGui.QFont.Bold))
+        self.labelTOTAL = QtGui.QLabel('TOTAL')
+        self.labelTOTAL.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelTOTAL.setFont(QtGui.QFont("Arial", 48, QtGui.QFont.Bold))
+
+         
+        self.layout = QtGui.QGridLayout()
+        self.layout.setSpacing(1)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        
+        self.layout.addWidget(self.nav)
+        self.layout.addWidget(self.canvas)
+        
+        self.hboxFRAC = QtGui.QHBoxLayout()
+        self.hboxFRAC.addWidget(self.labelFRAC)
+        self.hboxFRAC.addWidget(self.lcdFRAC)        
+        self.hboxGND = QtGui.QHBoxLayout()
+        self.hboxGND.addWidget(self.labelGND)
+        self.hboxGND.addWidget(self.lcdGND)
+        self.hboxEXC = QtGui.QHBoxLayout()
+        self.hboxEXC.addWidget(self.labelEXC)
+        self.hboxEXC.addWidget(self.lcdEXC)
+        self.hboxBAC = QtGui.QHBoxLayout()
+        self.hboxBAC.addWidget(self.labelBAC)
+        self.hboxBAC.addWidget(self.lcdBAC)
+        self.hboxTOTAL = QtGui.QHBoxLayout()
+        self.hboxTOTAL.addWidget(self.labelTOTAL)
+        self.hboxTOTAL.addWidget(self.lcdTOTAL)
+        
+
+        self.layout.addItem(self.hboxFRAC)
+        self.layout.addItem(self.hboxGND)
+        self.layout.addItem(self.hboxEXC)
+        self.layout.addItem(self.hboxBAC)
+        self.layout.addItem(self.hboxTOTAL)
+
+
+        self.setLayout(self.layout)
+       
+        width = self.canvas.width() + 500
+        height = self.nav.height() + self.canvas.height() + 8*self.lcdGND.height() + 200
+        self.setFixedSize(width, height)
+        self.setWindowTitle('PMT_trace_viewer')
+        
+                   
+    def replot(self, rel_data_path):
+        abs_data_path = os.path.join(self.data_dir, rel_data_path) + '.hdf5'
+        with h5py.File(abs_data_path) as h5f:
+            gnd = h5f['gnd']
+            exc = h5f['exc']
+            bac = h5f['bac']
+            self.canvas.ax.clear()
+            self.canvas.ax.plot(gnd, label='gnd')
+            self.canvas.ax.plot(exc, label='exc')
+            self.canvas.ax.plot(bac, label='bac')
+            self.canvas.ax.legend()
+            gnd = np.mean(h5f['gnd'][3000:23000])
+            exc = np.mean(h5f['exc'][3000:23000])
+            bac = np.mean(h5f['bac'][3000:23000])
+            tot = (gnd+exc-2*bac)
+            if tot==0:
+                frac = 0
+            else:
+                frac = (exc-bac)/tot
+            self.lcdGND.display(str(np.round(gnd,3)))
+            self.lcdEXC.display(str(np.round(exc,3)))
+            self.lcdBAC.display(str(np.round(bac,3)))
+            self.lcdFRAC.display(str(np.round(frac,3)))
+            self.lcdTOTAL.display(str(np.round(tot,3)))
+        self.canvas.draw()
+                            
+
+    
